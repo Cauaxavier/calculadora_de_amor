024474acc59561d0e47bc2959658f09e72ef2258
Cauã Gomes Xavier <114945046+Cauaxavier@users.noreply.github.com>
2023-05-20 22:58:03 -0300

    estilizando a parte de cima do app

--- interface.py
+++ interface.py
@@ -28,7 +28,24 @@ frame_cima.grid(row=0, column=0)
 frame_baixo = Frame(janela, width=510, height=250, bg=cor1, relief='solid')
 frame_baixo.grid(row=1, column=0)
 
-logo = Label(frame_cima, width=500, text='Calculadora de amor', font=('Fixedsys 22'), bg=cor7, fg=cor1, padx=100, anchor='nw')
+logo = Label(frame_cima, width=500, text='Calculadora de amor', font=('Fixedsys 25'), bg=cor7, fg=cor1, padx=80, anchor='nw')
 logo.place(x=0, y=0)
+
+imagem = Image.open('imagens/coracao3.png')
+imagem = imagem.resize((150,150))
+imagem = ImageTk.PhotoImage(imagem)
+
+mostrar_imagem = Label(frame_cima, image=imagem, width=950, compound=LEFT, bg=cor1, fg=cor4, padx=5, anchor='nw')
+mostrar_imagem.place(x=20, y=60)
+
+msg = 'porcentagem de amor entre'
+resultado1 = Label(frame_cima, width=65, text=msg, padx=20, anchor='nw', font='Verdana 12 bold ', bg=cor1, fg=cor0)
+resultado1.place(x=180, y=80)
+
+resultado2 = Label(frame_cima, width=25, text='resultado', padx=1, anchor=CENTER, font='Verdana 12 bold', bg=cor1, fg=cor5)
+resultado2.place(x=180, y=110)
+
+resultado3 = Label(frame_cima, width=10, text='95%', padx=10, anchor=CENTER, font='Verdana 25 bold', bg=cor1, fg=cor0)
+resultado3.place(x=220, y=150)
 
 janela.mainloop()
